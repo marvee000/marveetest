fb37872bd852f8a68b47051897792c767b4d5701
marvee <mawolor@gmail.com>
2022-04-12 09:45:45 -0700

    echo "# marvee_test" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/marvee000/marvee_test.git git push -u origin main

---
+++ marvee.py
@@ -0,0 +1,16 @@
+fullname = input('what is your name? ')
+print('hi ' + fullname)
+name = input('what is your fullname? ')
+print('hi ' + name)
+favorite = input('what is your favorite color? ')
+print(name + ' likes ' + favorite)
+birth_year = input('birth year: ')
+age = 2022 - int(birth_year)
+print(age)
+weight_lbs = input('what is your weight_lbs: ')
+weight_kgs = int(weight_lbs) * 0.45
+print(weight_kgs)
+print(name + ' is ' + str(weight_kgs) + 'kg')
+
+
+
